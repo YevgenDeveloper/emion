00f2ff6215972fd3a35ca72607bf3afb2ce843aa
Yevgen <eugen.developer.it@gmail.com>
2021-12-25 00:25:15 +0000

    ✅ - Extends coverage

--- src/environmentsRunner.test.ts
+++ src/environmentsRunner.test.ts
@@ -1,38 +1,232 @@
+import ChildProcessEnventsHandler from '@/childProcessEventsHandlers'
+import { IExecutionEnvironement, IRepositoryConfiguration } from '@/configuration.interface'
+import ConfigurationHandler from '@/configurationHandler'
+import EnvironmentsRunner from '@/environmentsRunner'
 import test from 'ava'
-import sinon from 'sinon'
+import sinon, { stub } from 'sinon'
 import Vorpal from 'vorpal'
-import { IExecutionEnvironement, IRepositoryConfiguration } from './configuration.interface'
-import ConfigurationHandler from './configurationHandler'
-import EnvironmentsRunner from './environmentsRunner'
 test('Should return a runner from the static method', (t) => {
   const result = EnvironmentsRunner.getEnvironmentsRunner()
   t.is(result.constructor.name, 'EnvironmentsRunner')
 })
-test('Should log the correct information while executing an environement (TO BE COMPLETED TO MANAGE ASYNC)', async (t) => {
-  const log = sinon.fake.returns(undefined)
-  Vorpal.prototype.log = log
-  const testEnv: IExecutionEnvironement = {
+function plugInStubs({ env, repo, execFunction, doNotStubExecuteEnvironement }: {
+  env?: IExecutionEnvironement,
+  repo?: IRepositoryConfiguration,
+  execFunction?: () => Promise<number>,
+  doNotStubExecuteEnvironement?: boolean
+}) {
+  const stubs: any = {
+  }
+  if (execFunction) {
+    stubs.execute = stub(ChildProcessEnventsHandler.prototype, 'execute')
+      .callsFake(execFunction)
+  } else {
+    stubs.execute = stub(ChildProcessEnventsHandler.prototype, 'execute')
+      .callsFake(async function f(this: ChildProcessEnventsHandler) {
+        if (this.onInit) {
+          await this.onInit()
+        }
+        await this.onNewDataFromStandardOutput('Standard Output', 1)
+        await this.onNewDataFromErrorOutput('Error Output', 1)
+        await this.onExit(0, 1)
+        return 1
+      })
+  }
+  stubs.log = stub(Vorpal.prototype, 'log').returnsThis()
+  const testEnv: IExecutionEnvironement = env || {
     command: 'echo "Haha" && exit 0',
     id: 'testEnv',
     repo: 'testRepo'
   }
-  ConfigurationHandler.prototype.getEnvironment = sinon.fake.returns(testEnv)
-  const testRepo: IRepositoryConfiguration = {
+  stubs.getEnvironment = stub(ConfigurationHandler.prototype, 'getEnvironment').returns(testEnv)
+  const testRepo: IRepositoryConfiguration = repo || {
     environments: {
       testEnv
     },
     initCommand: 'echo "Huhu" && exit 0'
   }
-  ConfigurationHandler.prototype.getRepository = sinon.fake.returns(testRepo)
-  ConfigurationHandler.prototype.getRepoPath = sinon.fake.returns('/')
+  stubs.getRepository = stub(ConfigurationHandler.prototype, 'getRepository').returns(testRepo)
+  stubs.getRepoPath = stub(ConfigurationHandler.prototype, 'getRepoPath').returns('/')
+  if (!doNotStubExecuteEnvironement) {
+    stubs.executeEnvironement = stub(EnvironmentsRunner.prototype, 'executeEnvironement').resolves(undefined)
+  }
+  return stubs
+}
+function restoreMethods(stubs: {
+  [key: string]: sinon.SinonStub
+}) {
+  const keys = Object.keys(stubs)
+  keys.forEach((key) => stubs[key].restore())
+}
+test.serial('Should log the correct information while running an environement', async (t) => {
+  const stubs = plugInStubs({})
   const v: Vorpal = new Vorpal()
   const runner = EnvironmentsRunner.getEnvironmentsRunner()
   runner.initialize(v)
-  runner.runEnvironement('testEnv')
+  await runner.runEnvironement('testEnv')
   const ansiRegex = /[\u001b\u009b][[()#;?]*(?:[0-9]{1,4}(?:;[0-9]{0,4})*)?[0-9A-ORZcf-nqry=><]/g
-  t.is(log.getCall(0).args.length, 1)
-  t.is(log.getCall(0).args[0].replace(ansiRegex, ''), '[testEnv] STARTING ENVIRONMENT')
-  t.is(log.getCall(1).args.length, 1)
-  t.is(log.getCall(1).args[0].replace(ansiRegex, ''), '[testEnv] INITIALISATION - START')
+  t.is(stubs.log.getCall(0).args.length, 1)
+  t.is(stubs.log.getCall(0).args[0].replace(ansiRegex, ''), '[testEnv] STARTING ENVIRONMENT')
+  t.is(stubs.log.getCall(1).args.length, 1)
+  t.is(stubs.log.getCall(1).args[0].replace(ansiRegex, ''), '[testEnv] INITIALISATION - START')
+  t.is(stubs.log.getCall(2).args.length, 1)
+  t.is(stubs.log.getCall(2).args[0].replace(ansiRegex, ''), '[testEnv] Standard Output')
+  t.is(stubs.log.getCall(3).args.length, 1)
+  t.is(stubs.log.getCall(3).args[0].replace(ansiRegex, ''), '[testEnv] Error Output')
+  t.is(stubs.log.getCall(4).args.length, 1)
+  t.is(stubs.log.getCall(4).args[0].replace(ansiRegex, ''), '[testEnv] INITIALISATION - END')
   t.pass()
+  restoreMethods(stubs)
+})
+test.serial('Should log the correct information while running an environement with no init command', async (t) => {
+  const testEnv = {
+    command: 'echo "Haha" && exit 0',
+    id: 'testEnv',
+    repo: 'testRepo',
+    currentPid: 1
+  }
+  const stubs = plugInStubs({
+    repo: {
+      environments: {
+        testEnv
+      }
+    }
+  })
+  const v: Vorpal = new Vorpal()
+  const runner = EnvironmentsRunner.getEnvironmentsRunner()
+  runner.initialize(v)
+  await runner.runEnvironement('testEnv')
+  const ansiRegex = /[\u001b\u009b][[()#;?]*(?:[0-9]{1,4}(?:;[0-9]{0,4})*)?[0-9A-ORZcf-nqry=><]/g
+  t.is(stubs.log.getCall(0).args.length, 1)
+  t.is(stubs.log.getCall(0).args[0].replace(ansiRegex, ''), '[testEnv] STARTING ENVIRONMENT')
+  t.pass()
+  restoreMethods(stubs)
+})
+test.serial('Should log the correct information while trying to run an already running environement', async (t) => {
+  const stubs = plugInStubs({
+    env: {
+      command: 'echo "Haha" && exit 0',
+      id: 'testEnv',
+      repo: 'testRepo',
+      currentPid: 1
+    },
+    execFunction: async function f(this: ChildProcessEnventsHandler) {
+      this.onInit()
+      this.onNewDataFromStandardOutput('Standard Output', 1)
+      this.onNewDataFromErrorOutput('Error Output', 1)
+      this.onExit(0, 1)
+      return 1
+    }
+  })
+  const v: Vorpal = new Vorpal()
+  const runner = EnvironmentsRunner.getEnvironmentsRunner()
+  runner.initialize(v)
+  await runner.runEnvironement('testEnv')
+  const ansiRegex = /[\u001b\u009b][[()#;?]*(?:[0-9]{1,4}(?:;[0-9]{0,4})*)?[0-9A-ORZcf-nqry=><]/g
+  t.is(stubs.log.getCall(0).args.length, 1)
+  t.is(stubs.log.getCall(0).args[0].replace(ansiRegex, ''), '[testEnv] ENVIRONMENT IS ALREADY RUNNING WITH PID 1')
+  t.pass()
+  restoreMethods(stubs)
+})
+test.serial('Should log the correct information while executing an environement', async (t) => {
+  const testEnv: IExecutionEnvironement = {
+    command: 'echo "Haha" && exit 0',
+    id: 'testEnv',
+    repo: 'testRepo'
+  }
+  const stubs = plugInStubs({
+    doNotStubExecuteEnvironement: true
+  })
+  const v: Vorpal = new Vorpal()
+  const runner = EnvironmentsRunner.getEnvironmentsRunner()
+  runner.initialize(v)
+  await runner.executeEnvironement({ env: testEnv, color: 'red' })
+  const ansiRegex = /[\u001b\u009b][[()#;?]*(?:[0-9]{1,4}(?:;[0-9]{0,4})*)?[0-9A-ORZcf-nqry=><]/g
+  t.is(stubs.log.getCall(0).args.length, 1)
+  t.is(stubs.log.getCall(0).args[0].replace(ansiRegex, ''), '[testEnv] Standard Output')
+  t.is(stubs.log.getCall(1).args.length, 1)
+  t.is(stubs.log.getCall(1).args[0].replace(ansiRegex, ''), '[testEnv] Error Output')
+  t.pass()
+  restoreMethods(stubs)
+})
+test.serial('Should log the correct information while executing an environement that waits for a console mesage', async (t) => {
+  const testEnv: IExecutionEnvironement = {
+    command: 'echo "Haha" && exit 0',
+    id: 'testEnv',
+    repo: 'testRepo',
+    readyWhen: {
+      consoleMessageIsFound: 'READY on port ${port}'
+    }
+  }
+  const stubs = plugInStubs({
+    doNotStubExecuteEnvironement: true,
+    env: testEnv,
+    execFunction: async function f(this: ChildProcessEnventsHandler) {
+      if (this.onInit) {
+        await this.onInit()
+      }
+      await this.onNewDataFromStandardOutput('Standard Output 1', 1)
+      await this.onNewDataFromStandardOutput('READY on port 1234', 1)
+      await this.onNewDataFromStandardOutput('Standard Output 2', 1)
+      await this.onExit(0, 1)
+      return 1
+    }
+  })
+  const v: Vorpal = new Vorpal()
+  const runner = EnvironmentsRunner.getEnvironmentsRunner()
+  runner.initialize(v)
+  await runner.executeEnvironement({ env: testEnv })
+  const ansiRegex = /[\u001b\u009b][[()#;?]*(?:[0-9]{1,4}(?:;[0-9]{0,4})*)?[0-9A-ORZcf-nqry=><]/g
+  t.is(stubs.log.getCall(0).args.length, 1)
+  t.is(stubs.log.getCall(0).args[0].replace(ansiRegex, ''), '[testEnv] Standard Output 1')
+  t.is(stubs.log.getCall(1).args.length, 1)
+  t.is(stubs.log.getCall(1).args[0].replace(ansiRegex, ''), '[testEnv] READY on port 1234')
+  t.is(stubs.log.getCall(2).args.length, 1)
+  t.is(stubs.log.getCall(2).args[0].replace(ansiRegex, ''), '[testEnv] ENVIRONMENT READY ON PORT 1234')
+  t.is(stubs.log.getCall(3).args.length, 1)
+  t.is(stubs.log.getCall(3).args[0].replace(ansiRegex, ''), '[testEnv] Standard Output 2')
+  t.pass()
+  restoreMethods(stubs)
+})
+test.serial('Should log the correct information while executing an environement that waits for a port to be up', async (t) => {
+  const testEnv: IExecutionEnvironement = {
+    command: 'echo "Haha" && exit 0',
+    id: 'testEnv',
+    repo: 'testRepo',
+    readyWhen: {
+      portIsUp: 1234
+    }
+  }
+  const stubs = plugInStubs({
+    doNotStubExecuteEnvironement: true,
+    env: testEnv,
+    execFunction: async function f(this: ChildProcessEnventsHandler) {
+      if (this.onInit) {
+        await this.onInit()
+      }
+      await this.onNewDataFromStandardOutput('Standard Output 1', 1)
+      await this.onNewDataFromStandardOutput('Standard Output 2', 1)
+      await this.onExit(0, 1)
+      return 1
+    }
+  })
+  stubs.stubbedModule = stub(
+    require.cache[require.resolve('portscanner')].exports,
+    'checkPortStatus')
+    .resolves('closed')
+  delete require.cache[require.resolve('@/environmentsRunner')]
+  const EnvironmentsRunnerTemp = require('@/environmentsRunner').default
+  const v: Vorpal = new Vorpal()
+  const runner = EnvironmentsRunnerTemp.getEnvironmentsRunner()
+  runner.initialize(v)
+  await runner.executeEnvironement({ env: testEnv })
+  const ansiRegex = /[\u001b\u009b][[()#;?]*(?:[0-9]{1,4}(?:;[0-9]{0,4})*)?[0-9A-ORZcf-nqry=><]/g
+  t.is(stubs.log.getCall(0).args.length, 1)
+  t.is(stubs.log.getCall(0).args[0].replace(ansiRegex, ''), '[testEnv] Standard Output 1')
+  t.is(stubs.log.getCall(1).args.length, 1)
+  t.is(stubs.log.getCall(1).args[0].replace(ansiRegex, ''), '[testEnv] ENVIRONMENT READY ON PORT 1234')
+  t.is(stubs.log.getCall(2).args.length, 1)
+  t.is(stubs.log.getCall(2).args[0].replace(ansiRegex, ''), '[testEnv] Standard Output 2')
+  t.pass()
+  restoreMethods(stubs)
 })
--- src/environmentsRunner.ts
+++ src/environmentsRunner.ts
@@ -1,10 +1,10 @@
-import ChildProcess from 'child_process'
+import ChildProcessEnventsHandlerBuilder from '@/childProcessEventsHandlerBuilder'
+import { IExecutionEnvironement } from '@/configuration.interface'
+import { getConfigHandler } from '@/configurationHandler'
+import { checkAndparseParametrizedString, getEnvLoggerForEnvironement, kill, LogStartEnd, pickRandomColor } from '@/utils'
 import path from 'path'
 import * as portscanner from 'portscanner'
 import Vorpal from 'vorpal'
-import { IExecutionEnvironement } from './configuration.interface'
-import { getConfigHandler } from './configurationHandler'
-import { checkAndparseParametrizedString, getEnvLoggerForEnvironement, kill, LogStartEnd, pickRandomColor } from './utils'
 let runner: EnvironmentsRunner
 export default class EnvironmentsRunner {
   public static getEnvironmentsRunner() {
@@ -64,31 +64,33 @@ export default class EnvironmentsRunner {
           })
           const initCommand = getConfigHandler().getRepository(env.repo!).initCommand
           if (initCommand) {
-            this.logDataForEnv({
-              envName: env.id,
-              color,
-              data: 'INITIALISATION - START'
-            })
-            const cmd = ChildProcess.exec(initCommand, {
-              cwd: path.join(getConfigHandler().getRepoPath(), env.repo!)
-            })
-            cmd.stdout.on('data', (data: any) => {
-              this.logDataForEnv({ envName, color, data })
-            })
-            cmd.stderr.on('data', (data: any) => {
-              this.logDataForEnv({ envName, color, data })
-            })
-            cmd.on('exit', async (code: any) => {
-              this.logDataForEnv({
-                envName,
-                color,
-                data: 'INITIALISATION - END'
+            ChildProcessEnventsHandlerBuilder.aChildProcessEventsHandler()
+              .asExececutedProcess()
+              .withCommand(initCommand)
+              .withCommandCurrentWorkindDir(path.join(getConfigHandler().getRepoPath(), env.repo!))
+              .withOnInitCallBack(() => {
+                this.logDataForEnv({
+                  envName: env.id,
+                  color,
+                  data: 'INITIALISATION - START'
+                })
               })
-              resolve(this.executeEnvironement({ env, color }))
-            })
-            cmd.on('message', (message: any) => {
-              this.logDataForEnv({ envName, color, data: message })
-            })
+              .withOnNewDataFromStandardOutputCallBack((data) => {
+                this.logDataForEnv({ envName, color, data })
+              })
+              .withOnNewDataFromErrorOutputCallBack((data) => {
+                this.logDataForEnv({ envName, color, data })
+              })
+              .withOnExitCallBack((data) => {
+                this.logDataForEnv({
+                  envName,
+                  color,
+                  data: 'INITIALISATION - END'
+                })
+                resolve(this.executeEnvironement({ env, color }))
+              })
+              .build()
+              .execute()
           } else {
             resolve(this.executeEnvironement({ env, color }))
           }
@@ -109,68 +111,63 @@ export default class EnvironmentsRunner {
       if (env.dependsOn && env.dependsOn.length > 0) {
         await Promise.all(env.dependsOn.map((depEnv) => this.runEnvironement(depEnv)))
       }
-      const cmd = env.arguments
-        ? ChildProcess.spawn(env.command!, env.arguments, {
-          cwd: path.join(getConfigHandler().getRepoPath(), env.repo!)
-        })
-        : ChildProcess.exec(env.command!, {
-          cwd: path.join(getConfigHandler().getRepoPath(), env.repo!)
-        })
-      this.runningCommands[cmd.pid] = env
-      env.currentPid = cmd.pid
-      cmd.stdout.on('data', async (data: any) => {
-        this.logDataForEnv({ envName: env.id, color: pickedColor, data })
-        if (env.readyWhen) {
-          if (env.readyWhen.consoleMessageIsFound) {
-            const matchedValue = checkAndparseParametrizedString(
-              env.readyWhen.consoleMessageIsFound,
-              data
-            )
-            if (matchedValue !== null) {
-              this.logDataForEnv({
-                envName: env.id,
-                color: pickedColor,
-                data: `ENVIRONMENT READY ON PORT ${matchedValue.port}`
-              }
+      let builder = ChildProcessEnventsHandlerBuilder.aChildProcessEventsHandler()
+      builder = env.arguments ? builder.asSpawnedProcess() : builder.asExececutedProcess()
+      const pid = await builder
+        .withCommand(env.command!)
+        .withCommandArguments(env.arguments)
+        .withCommandCurrentWorkindDir(path.join(getConfigHandler().getRepoPath(), env.repo!))
+        .withOnNewDataFromStandardOutputCallBack(async (data) => {
+          this.logDataForEnv({ envName: env.id, color: pickedColor, data })
+          if (env.readyWhen) {
+            if (env.readyWhen.consoleMessageIsFound && !env.ready) {
+              const matchedValue = checkAndparseParametrizedString(
+                env.readyWhen.consoleMessageIsFound,
+                data
               )
-              resolve()
+              if (matchedValue !== null) {
+                this.logDataForEnv({
+                  envName: env.id,
+                  color: pickedColor,
+                  data: `ENVIRONMENT READY ON PORT ${matchedValue.port}`
+                }
+                )
+                env.ready = true
+                resolve()
+              }
+            }
+            if (env.readyWhen.portIsUp && !env.ready) {
+              const status = await portscanner.checkPortStatus(
+                env.readyWhen.portIsUp
+              )
+              if (status === 'closed') {
+                this.logDataForEnv({
+                  envName: env.id,
+                  color: pickedColor,
+                  data: `ENVIRONMENT READY ON PORT ${env.readyWhen.portIsUp}`
+                }
+                )
+                env.ready = true
+                resolve()
+              }
             }
           }
-          if (env.readyWhen.portIsUp) {
-            const status = await portscanner.checkPortStatus(
-              env.readyWhen.portIsUp
-            )
-            if (status === 'closed') {
-              this.logDataForEnv({
-                envName: env.id,
-                color: pickedColor,
-                data: `ENVIRONMENT READY ON PORT ${env.readyWhen.portIsUp}`
-              }
-              )
-              resolve()
-            }
+        })
+        .withOnNewDataFromErrorOutputCallBack((data) => {
+          this.logDataForEnv({ envName: env.id, pickedColor, data })
+        })
+        .withOnExitCallBack((code, currentPid) => {
+          if (this.runningCommands[currentPid]) {
+            this.logDataForEnv({ envName: env.id, pickedColor, data: `ENVIRONMENT ENDED WITH CODE ${code}` })
           }
-        }
-      })
-      cmd.stderr.on('data', (data: any) => {
-        this.logDataForEnv({ envName: env.id, pickedColor, data })
-      })
-      cmd.on('exit', (code: any, ...args) => {
-        if (this.runningCommands[cmd.pid]) {
-          this.logDataForEnv({ envName: env.id, pickedColor, data: `ENVIRONMENT ENDED WITH CODE ${code}` })
-        }
-        resolve(code)
-        delete env.currentPid
-        delete this.runningCommands[cmd.pid]
-      })
-      cmd.on('close', (code: any) => {
-        if (this.runningCommands[cmd.pid]) {
-          this.logDataForEnv({ envName: env.id, pickedColor, data: `ENVIRONMENT ENDED WITH CODE ${code}` })
-        }
-        resolve(code)
-        delete env.currentPid
-        delete this.runningCommands[cmd.pid]
-      })
+          resolve(code)
+          delete env.currentPid
+          delete this.runningCommands[currentPid]
+        })
+        .build()
+        .execute()
+      this.runningCommands[pid] = env
+      env.currentPid = pid
     })
   }
 }
